132d0edfd0493858adb012dc2a836dcea22949f8
flavin-dev <flavinml07@gmail.com>
2026-07-25 22:03:39 +0530

    Time: 0 ms (100%), Space: 9 MB (35.21%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.cpp
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     int lengthOfLastWord(string s) {
         int cnt=0;
-        bool one=true;
         for(int i=s.size()-1;i>=0;i--){
             if(s[i]==' '){
                 if(cnt>0){
