17ffe7474ec5f31a3aa647fa70e3a1961c1df04f
flavin-dev <flavinml07@gmail.com>
2026-08-11 20:20:49 +0530

    0217-CONTAINS-DUPLICATE Stats: Time: 32 ms (79.94%), Space: 82.7 MB (91.26%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,12 +1,11 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        unordered_map<int,int>mp;
-        for(int i=0;i<nums.size();i++){
-            if(mp.find(nums[i])!=mp.end()){
+        sort(nums.begin(),nums.end());
+        for(int i=1;i<nums.size();i++){
+            if(nums[i]==nums[i-1]){
                 return true;
             }
-            mp[nums[i]]=i;
         }
         return false;
     }
