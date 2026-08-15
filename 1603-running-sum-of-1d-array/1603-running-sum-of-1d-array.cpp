edb13db89dc10bffbe869e09c742b081a246a2e4
flavin-dev <flavinml07@gmail.com>
2026-08-15 19:56:37 +0530

    1603-RUNNING-SUM-OF-1D-ARRAY Stats: Time: 0 ms (100%), Space: 12.5 MB (82.09%) - LeetHub

--- 1603-running-sum-of-1d-array/1603-running-sum-of-1d-array.cpp
+++ 1603-running-sum-of-1d-array/1603-running-sum-of-1d-array.cpp
@@ -1,8 +1,7 @@
 class Solution {
 public:
     vector<int> runningSum(vector<int>& nums) {
-        int N=nums.size();
-        for(int i=1;i<N;i++){
+        for(int i=1;i<nums.size();i++){
             nums[i]+=nums[i-1];
         }
         return nums;
