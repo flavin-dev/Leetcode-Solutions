52fca4df9ecd3b233c0cd1ea07b76d8b10dbd4d3
flavin-dev <flavinml07@gmail.com>
2026-08-15 19:40:56 +0530

    0792-BINARY-SEARCH Stats: Time: 0 ms (100%), Space: 31.3 MB (82.09%) - LeetHub

--- 0792-binary-search/0792-binary-search.cpp
+++ 0792-binary-search/0792-binary-search.cpp
@@ -3,15 +3,16 @@ public:
     int search(vector<int>& nums, int target) {
         int l=0,r=nums.size()-1;
         while(l<=r){
-            int mid=(l+r)/2;
-            if(nums[mid]==target){
-                return mid;
+            int i=(l+r)/2;
+            if(target<nums[i]){
+                r=i-1;
             }
-            if(nums[mid]>target){
-                r=mid-1;
+            else if(target > nums[i]){
+                l=i+1;
+
             }
             else{
-                l=mid+1;
+                return i;
             }
         }
         return -1;
