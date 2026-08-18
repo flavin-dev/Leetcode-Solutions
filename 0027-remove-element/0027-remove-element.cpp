9cbcff48b220589d63f85277ee80bef33a41e872
flavin-dev <flavinml07@gmail.com>
2026-08-18 21:27:45 +0530

    0027-REMOVE-ELEMENT Stats: Time: 0 ms (100%), Space: 11.6 MB (84.82%) - LeetHub

--- 0027-remove-element/0027-remove-element.cpp
+++ 0027-remove-element/0027-remove-element.cpp
@@ -1,27 +1,14 @@
 class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
-        if(nums.empty()){
-            return 0;
+        int k=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]!=val){
+                nums[k]=nums[i];
+                k++;
+            }
+            
         }
-        int i=0,j=nums.size()-1;
-        while(i<=j){
-            if(nums[i]==val&&nums[j]!=val){
-                nums[i]=nums[j];
-                i++;
-                j--;
-            }
-            else if(nums[i]!=val&&nums[j]==val){
-                j--;
-            }
-            else if(nums[i]==val&&nums[j]==val){
-                j--;
-            }
-            else{
-                i++;
-            }
-        }
-        
-        return j+1;
+        return k;
     }
 };
