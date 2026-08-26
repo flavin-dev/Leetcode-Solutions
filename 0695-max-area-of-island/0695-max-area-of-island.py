5c5b99ae61506fe84f95600a82a91748f0c7c15e
flavin-dev <flavinml07@gmail.com>
2026-08-26 20:56:45 +0530

    Time: 38 ms (5.48%), Space: 19.8 MB (70.68%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.py
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -2,15 +2,27 @@ class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
         R,C=len(grid),len(grid[0])
         visit=set()
-        def dfs(row,col):
-            if (row<0 or row==R or col<0 or col==C or ((row,col) in visit) or grid[row][col]==0):
-                return 0
-            visit.add((row,col))
-            return (1+dfs(row+1,col)+dfs(row-1,col)+dfs(row,col+1)+dfs(row,col-1))
+        
+        maxcount=0
+        def dfs(r,c):
+            q=collections.deque()
+            visit.add((r,c))
+            q.append((r,c))
+            count=1
+            while q:
+                row,col=q.pop()
+                directions=[[1,0],[-1,0],[0,1],[0,-1]]
+                for dr,dc in directions:
+                    r,c=row+dr,col+dc
+                    if (r in range(R) and c in range(C) and grid[r][c]==1 and (r,c) not in visit):
+                        q.append((r,c))
+                        visit.add((r,c))
+                        count+=1
+            return count
 
-        area=0
+        
         for row in range(R):
             for col in range(C):
-                area=max(area,dfs(row,col))
-
-        return area
+                if grid[row][col]==1 and (row,col) not in visit:
+                    maxcount=max(maxcount,dfs(row,col))
+        return maxcount
